2e91b74c85ea8f7d499ab1e5ad6f6c79d081b00d
Maaike de Jong <maaikehdejong@gmail.com>
2022-11-22 10:11:09 +0100

    eerste commit

---
+++ src/main/java/nl/miwgroningen/ch10/maakekardia/libraryDemo/LibraryDemoApplication.java
@@ -0,0 +1,14 @@
+package nl.miwgroningen.ch10.maakekardia.libraryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+		LibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDemoApplication.class, args);
+	}
+
+}
